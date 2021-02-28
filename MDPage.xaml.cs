1810bd93004d77fdad2af085da1394f6a8a02d8c
Steff10740 <36547867+Steff10740@users.noreply.github.com>
2021-02-28 01:34:30 -0400

    Add files via upload

---
+++ MDPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PX3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MDPage : MasterDetailPage
+    {
+        public MDPage()
+        {
+            InitializeComponent();
+            
+        }
+
+    }
+}
---
+++ Register.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PX3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Register : ContentPage
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        async void Register2_Clicked(object sender, EventArgs e)
+        {
+            
+
+            if (string.IsNullOrEmpty(rName.Text))
+            {
+                await DisplayAlert("Advertencia", "Los campo no pueden estar vacios", "OK");
+                await Navigation.PushAsync(new Register());
+            }
+            
+            if (string.IsNullOrEmpty(rEmail.Text))
+            {
+                await DisplayAlert("Advertencia", "Los campo no pueden estar vacios", "OK");
+                 Navigation.PushAsync(new Register());
+
+            }
+            
+            if (string.IsNullOrEmpty(rPw.Text))
+            {
+                await DisplayAlert("Advertencia", "Los campo no pueden estar vacios", "OK");
+                 Navigation.PushAsync(new Register());
+            }
+            
+
+            if (string.IsNullOrEmpty(rCPw.Text))
+            {
+                await DisplayAlert("Advertencia", "Los campo no pueden estar vacios", "OK");
+                 Navigation.PushAsync(new Register());
+            }
+           
+            
+
+      
+            if (String.ReferenceEquals (rPw.Text, rCPw.Text))
+            {
+                await Navigation.PushAsync(new MDPage());
+            }
+            else
+            {
+                await DisplayAlert("Advertencia", "los campos deben ser iguales", "Ok");
+            }
+
+
+            string[] box = { rName.Text, rEmail.Text, rPw.Text, rCPw.Text };
+            foreach (string k in box)
+            {
+                if (!string.IsNullOrEmpty(rName.Text + rCPw.Text + rEmail.Text + rPw.Text))
+                {
+                    await Navigation.PushAsync(new MDPage());
+                }
+            }
+
+        }
+    }
+}
